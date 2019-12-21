d1c31eb26772c60ecececf41ab5b290438b4ceb1
Priya-300199 <priya181088@gmail.com>
2019-12-21 21:03:41 +0530

    config

--- PMWebAPI/PMWebAPI/App_Start/WebApiConfig.cs
+++ PMWebAPI/PMWebAPI/App_Start/WebApiConfig.cs
@@ -12,7 +12,7 @@ namespace PMWebAPI
         public static void Register(HttpConfiguration config)
         {
             //Enable CORS
-            config.EnableCors(new EnableCorsAttribute(origins: "http://localhost", headers: "*", methods: "*"));
+            config.EnableCors(new EnableCorsAttribute(origins: "http://localhost/ProjectManager/index.html", headers: "*", methods: "*"));
 
             // Web API configuration and services
 
